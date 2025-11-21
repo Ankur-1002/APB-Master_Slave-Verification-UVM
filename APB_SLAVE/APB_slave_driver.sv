ed6f681848265e641eb22fa0c2b10ddb47e862f0
Ankur Sinha <83006411+Ankur-1002@users.noreply.github.com>
2025-11-21 15:15:02 +0530

    Update APB_slave_driver.sv

--- APB_SLAVE/APB_slave_driver.sv
+++ APB_SLAVE/APB_slave_driver.sv
@@ -1,1 +1,22 @@
 
+class apb_slave_driver extends uvm_driver;
+	`uvm_component_utils(apb_slave_driver)
+
+		function new(string name = "apb_slave_driver", uvm_component parent);
+			super.new(name, parent);
+		endfunction
+
+		function void build_phase(uvm_phase phase);
+			super.build_phase(phase);
+		endfunction
+
+		function void connect_phase(uvm_phase phase);
+			super.connect_phase(phase);
+		endfunction
+
+		task run_phase(uvm_phase phase);
+			super.run_phase(phase);
+		//logic
+		endtask
+
+endclass
