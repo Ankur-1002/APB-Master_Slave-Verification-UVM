d3e7f0e24bf81f7b117f0fe5b4bf9a2afa61f840
Ankur Sinha <83006411+Ankur-1002@users.noreply.github.com>
2025-11-23 19:51:39 +0530

    Update apb_pkg.sv

--- apb_pkg.sv
+++ apb_pkg.sv
@@ -1,10 +1,19 @@
+//==========================================================================
+`include "APB_Define.sv"
+`include "APB_Slave_Intf.sv"
 package apb_pkg;
-    
-    //==========================================================================
-    // Global Parameters
-    //==========================================================================
-    `define ADDR_WIDTH = 32;
-    `define DATA_WIDTH = 32;  
-    `define MEM_DEPTH  = 256;
+ `include "uvm_macros.svh"
+  import uvm_pkg::*;
+ `include "APB_Slave_Txn.sv"
+ `include "APB_Slave_Sequence.sv"
+ `include "APB_Slave_Sequencer.sv"
+ `include "APB_Slave_Driver.sv"
+ `include "APB_Slave_Monitor.sv"
+ `include "APB_Slave_Agent.sv"
+ //`include "APB_reference_model.sv"
+ //`include "APB_Slave_Scoreboard.sv"
+ `include "APB_Slave_Env.sv"
+ `include "APB_Slave_Test.sv"
 
 endpackage: apb_pkg
+
