97922b8028835bbacf30e3bc1710c2efbf55bc8f
Ankur Sinha <83006411+Ankur-1002@users.noreply.github.com>
2025-11-23 23:12:45 +0530

    Create APB_Slave_Test.sv

---
+++ APB_SLAVE/ankur/APB_Slave_Test.sv
@@ -0,0 +1,19 @@
+class apb_test extends uvm_test;
+  `uvm_component_utils(apb_test)
+  apb_env env;
+  apb_sequence sequences;
+  function new(string name="apb_test",uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env=apb_env::type_id::create("env",this);
+    sequences=apb_sequence::type_id::create("sequences",this);
+  endfunction
+  task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    phase.raise_objection(this);
+    sequences.start(env.agent.sequencer);
+    phase.drop_objection(this);
+  endtask
+endclass
