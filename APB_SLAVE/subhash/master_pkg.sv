fe6653784c0edc77cf22e7c268ac287a3049bbd1
PADAMATA SUBHASH <103590978+Subhashpadamata@users.noreply.github.com>
2025-11-24 12:47:55 +0530

    gvv

---
+++ APB_SLAVE/subhash/master_pkg.sv
@@ -0,0 +1,14 @@
+package master_Pkg;
+	`include "uvm_pkg.sv"
+	import uvm_pkg::*;
+	`include "apb_slave.sv"
+	`include "master_tx.sv"
+	`include "master_intf.sv"
+	`include "master_seq_lib.sv"
+	`include "master_mon.sv"
+	`include "master_drv.sv"
+	`include "master_sqr.sv"
+	`include "master_agnt.sv"
+	`include "master_env.sv"
+	`include "master_test_lib.sv"
+endclass
