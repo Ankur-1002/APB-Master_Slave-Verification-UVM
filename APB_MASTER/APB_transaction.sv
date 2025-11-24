578958f23df4c92c5fba6caa00d377d944bdbe23
Ankur Sinha <83006411+Ankur-1002@users.noreply.github.com>
2025-11-24 12:39:22 +0530

    Update APB_transaction.sv

--- APB_MASTER/APB_transaction.sv
+++ APB_MASTER/APB_transaction.sv
@@ -1,17 +1,18 @@
-class apb_transaction extends uvm_sequence_item;
+class apb_master_txn extends uvm_sequence_item;
 
 	rand bit Penable;
 	rand bit Psel;
 	rand bit [DATA_WIDTH - 1:0] Paddr;
 	rand bit [ADDR_WIDTH - 1:0] Pwdata;
 	rand bit Pwrite;
-	rand bit [DATA_WIDTH - 1:0] Prdata;
-	rand bit Pready;
-	rand bit Pslverr;
+	rand bit transfer;
+		 bit [DATA_WIDTH - 1:0] Prdata;
+		 bit Pready;
+		 bit Pslverr;
 		
-	`uvm_object_utils(apb_transaction)
+	`uvm_object_utils(apb_master_txn)
 	
-	function new(string name = "APB_transaction", uvm_component parent);
+	function new(string name = "APB_master_txn", uvm_component parent);
 		super.new(name, parent);
 	endfunction
 
