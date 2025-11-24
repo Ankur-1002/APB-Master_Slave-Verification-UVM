ceb7435f19a694a536d310e4e63f02d102f31227
PADAMATA SUBHASH <103590978+Subhashpadamata@users.noreply.github.com>
2025-11-24 10:41:10 +0530

    files_change

--- APB_SLAVE/subhash/slave_sqr.sv
+++
@@ -1,8 +0,0 @@
-class slave_sqr extends uvm_sequencer#(slave_tx);
-	`uvm_component_utils(apb_slave_sequencer)
-
-	function new(string name = "slave_sqr", uvm_component parent);
-		super.new(name, parent);
-	endfunction
-
-endclass
